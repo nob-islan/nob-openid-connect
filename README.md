917d4324b85b7663b780e2ce09cfcf666b347654
higuchi-nob <higuchi-nob@iij.ad.jp>
2024-03-20 08:19:05 +0000

    readme復活

---
+++ README.md
@@ -0,0 +1,16 @@
+# Nob OpenID Connect
+
+標準的な OpenID Connect の**認可コードフロー**に従った認証機能をフルスクラッチで実装するプロジェクトです。
+
+## 設計書一覧
+
+- [業務シーケンス](./docs/Business-sequence.md)
+- [業務フロー](./docs/business-flow/README.md)
+- [エンドポイント](./docs/endpoint/README.md)
+- [API 定義](./docs/api/README.md)
+- [データベース定義](./docs/db/README.md)
+
+## WIP: コンポーネント一覧
+
+- リライングパーティ
+- OpenID プロバイダ
--- rp-web-project/src/components/login/LoginTop.tsx
+++ rp-web-project/src/components/login/LoginTop.tsx
@@ -1,25 +1,52 @@
+import { useState } from 'react';
 import styles from './LoginTop.module.scss';
 
+/**
+ * ログイン画面
+ *
+ */
 const LoginTop = () => {
+  /**
+   * ユーザ名を管理するstate
+   */
+  const [userName, setUserName] = useState();
+
+  /**
+   * パスワードを管理するstate
+   */
+  const [password, setPassword] = useState();
+
+  /**
+   * ログインAPIをコール
+   */
+  const submit = () => {
+    console.log(userName);
+    console.log(password);
+  };
+
   return (
     <div>
       <div className={styles.beginningMessage}>Nob OpenID Connect</div>
       <table>
-        <tr>
-          <td align="left">ユーザID:</td>
-          <td>
-            <input type="text"></input>
-          </td>
-        </tr>
-        <tr>
-          <td align="left">パスワード:</td>
-          <td>
-            <input type="password"></input>
-          </td>
-        </tr>
+        <tbody>
+          <tr>
+            <td align="left">ユーザID:</td>
+            <td>
+              <input type="text"></input>
+            </td>
+          </tr>
+          <tr>
+            <td align="left">パスワード:</td>
+            <td>
+              <input type="password"></input>
+            </td>
+          </tr>
+        </tbody>
       </table>
       <div className={styles.buttonWrapper}>
-        <button className={styles.button}>ログイン</button>
+        <button className={styles.button} onClick={submit}>
+          ログイン
+        </button>
       </div>
     </div>
   );
