55c379f7157b24c0cd515756a1d8eb28848f0bfd
higuchi-nob <higuchi-nob@iij.ad.jp>
2024-03-30 06:29:20 +0000

    パッケージ構成記載

--- op-app-project/src/main/java/nob/example/opappproject/entity/UserInfo.java
+++ op-app-project/src/main/java/nob/example/opappproject/entity/UserInfo.java
@@ -1,27 +1,37 @@
 package nob.example.opappproject.entity;
 
 import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /**
  * user_infoテーブル向けエンティティクラスです。
  * 
  * @author nob
  */
+@Entity
+@Table(name = "user_info")
 @Data
 public class UserInfo {
 
     /**
      * ユーザID
      */
+    @Id
+    @Column(length = 10, nullable = false)
     private String userId;
 
     /**
      * パスワード
      */
+    @Column(length = 32, nullable = false)
     private String password;
 
     /**
      * ユーザ名
      */
+    @Column(length = 10, nullable = false)
     private String userName;
 }
