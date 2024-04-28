7b9ad146460bd6453508604d2c9e4f07e8cf38da
higuchi-nob <higuchi-nob@iij.ad.jp>
2024-04-28 09:32:29 +0000

    javadoc微修正

--- rp-app-project/src/main/java/nob/example/rpappproject/rest/constants/UrlConst.java
+++ rp-app-project/src/main/java/nob/example/rpappproject/rest/constants/UrlConst.java
@@ -13,22 +13,22 @@ public class UrlConst {
     public static final String OP_APP_ORIGIN = "http://localhost:8081";
 
     /**
-     * ベースURL
+     * OP ベースURL
      */
     public static final String BASE_URL = "/api/op";
 
     /**
-     * 認可API
+     * OP 認可API
      */
     public static final String AUTHORIZATION = "/authorization";
 
     /**
-     * トークン発行API
+     * OP トークン発行API
      */
     public static final String TOKEN = "/token";
 
     /**
-     * UserInfo取得API
+     * OP UserInfo取得API
      */
     public static final String USERINFO = "/userinfo";
 }
