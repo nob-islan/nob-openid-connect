8a0339e2f51a0d60158090b5d7913b1f38481a23
higuchi-nob <higuchi-nob@iij.ad.jp>
2024-04-28 09:07:04 +0000

    rp-web-project init

---
+++ rp-web-project/src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Routes></Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ rp-web-project/src/reducers/index.ts
@@ -0,0 +1,8 @@
+import { reducer as FormReducer } from 'redux-form';
+
+/**
+ * reducer一覧
+ */
+export const reducers = {
+  form: FormReducer
+};
