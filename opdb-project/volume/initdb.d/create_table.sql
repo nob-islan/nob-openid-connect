acc80f6aca1ec734e75c9bc8028577e58c32106c
higuchi-nob <higuchi-nob@iij.ad.jp>
2024-05-06 05:46:52 +0000

    スキーマ追加

--- opdb-project/volume/initdb.d/create_table.sql
+++ opdb-project/volume/initdb.d/create_table.sql
@@ -2,11 +2,19 @@
 CREATE DATABASE OPDB;
 USE OPDB;
 
--- テーブル作成
+-- テーブル作成 user_info
 CREATE TABLE IF NOT EXISTS user_info (
     user_id VARCHAR(10) NOT NULL
     , password VARCHAR(32) NOT NULL
     , PRIMARY KEY(user_id)
+);
+
+-- TODO テーブル作成 client_info
+CREATE TABLE IF NOT EXISTS client_info (
+    client_id VARCHAR(10) NOT NULL
+    , client_secret VARCHAR(32) NOT NULL
+    , redirect_uri TEXT NOT NULL
+    , PRIMARY KEY(client_id)
 );
 
 -- user_info投入
@@ -18,4 +26,15 @@ LOAD DATA LOCAL INFILE '/csv/user_info.csv'
     LINES
         TERMINATED BY '\n'
     IGNORE 1 ROWS
-        (user_id, password); -- DBのどのカラムに相当するかを明記
+        (user_id, password);
+
+-- TODO client_info投入
+LOAD DATA LOCAL INFILE '/csv/client_info.csv'
+    INTO TABLE
+        client_info
+    FIELDS
+        TERMINATED BY ','
+    LINES
+        TERMINATED BY '\n'
+    IGNORE 1 ROWS
+        (client_id, client_secret, redirect_uri);
