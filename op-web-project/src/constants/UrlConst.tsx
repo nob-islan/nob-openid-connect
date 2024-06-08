7609935c42c1d42e33fddd431abaa4501aabdf9c
nob-islan <nob.snail@gmail.com>
2024-06-08 13:11:56 +0000

    不要な定数削除

--- op-web-project/src/constants/UrlConst.tsx
+++ op-web-project/src/constants/UrlConst.tsx
@@ -20,16 +20,6 @@ class UrlConst {
    */
   static readonly CERTIFICATION =
     this.OP_APP_DOMAIN + this.OP_APP_BASE_URL + '/certification';
-
-  /**
-   * OpenIDプロバイダWebのドメイン
-   */
-  static readonly OP_WEB_DOMAIN = 'http://localhost:3001';
-
-  /**
-   * 認証・ユーザ情報提供同意画面
-   */
-  static readonly LOGIN = this.OP_WEB_DOMAIN + '/login';
 }
 
 export default UrlConst;
