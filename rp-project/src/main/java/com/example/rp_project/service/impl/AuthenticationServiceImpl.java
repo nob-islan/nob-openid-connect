2e5b8116dbb29eb326f477d00549a4c55b069979
nob-islan <nob.snail@gmail.com>
2024-12-29 06:22:04 +0000

    URI削除

--- rp-project/src/main/java/com/example/rp_project/service/impl/AuthenticationServiceImpl.java
+++ rp-project/src/main/java/com/example/rp_project/service/impl/AuthenticationServiceImpl.java
@@ -1,7 +1,5 @@
 package com.example.rp_project.service.impl;
 
-import java.net.URI;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -9,7 +7,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.rp_project.op.constant.OpUrlConstant;
 import com.example.rp_project.op.reqres.OpFetchTokenRequest;
